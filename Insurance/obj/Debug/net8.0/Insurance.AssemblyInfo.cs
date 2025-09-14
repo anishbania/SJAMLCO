3e1d72db78750ee53797466c0261768eeec4f9eb
unknown <ps882194@gmail.com>
2025-09-14 11:57:39 +0545

    initialized

--- Insurance/obj/Debug/net8.0/Insurance.AssemblyInfo.cs
+++ Insurance/obj/Debug/net8.0/Insurance.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Insurance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30da5012f8f3a273b3868dba11dbebc4165c69e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcbbf5f8d00fa5e4ec807426b027ada331242fbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Insurance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Insurance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
